a0549068ada77e0b4f7ef64145bd128e700c9776
Jay Cole <jdotcole233@gmail.com>
2023-03-20 10:19:01 +0000

    christopher

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *  addTwoNumbers - Adds two numbers
+ *  @a: first param
+ *  @b: second param
+ *  Return: return the sum of a and b
+ */
+
+int addTwoNumbers(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	return (0);
+}
